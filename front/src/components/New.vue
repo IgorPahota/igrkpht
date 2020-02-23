b1a8b2474d983800c7a7bba90315266ec4305ccf
Igor Pahota <eto.pochtovaya@gmail.com>
2020-02-23 20:10:08 +0300

    base skeleton

---
+++ front/src/components/New.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button>CLick me!</button>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "New",
+        props: {
+            text: String
+        },
+        data() {
+            return
+        }
+
+    }
+</script>
+
+<style scoped>
+div {
+    background-color: white;
+}
+</style>
